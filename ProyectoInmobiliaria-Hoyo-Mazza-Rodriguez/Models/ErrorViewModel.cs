ad4dcc0ae7c74e9599bbc023e5a8357820ccea4a
JuanCRodriguez0 <juancruzrodriguez0@gmail.com>
2026-08-13 16:22:24 -0300

    Creación proyeto

---
+++ ProyectoInmobiliaria-Hoyo-Mazza-Rodriguez/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoInmobiliaria_Hoyo_Mazza_Rodriguez.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
